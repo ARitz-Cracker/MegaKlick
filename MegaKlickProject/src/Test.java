23bca9e44d12187dac9ee7fc91a6842ca358a169
lamentable <henrywu2006@gmail.com>
2015-04-27 11:31:42 -0400

    Concept test

--- MegaKlickProject/src/Test.java
+++ MegaKlickProject/src/Test.java
@@ -1,14 +1,24 @@
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
+import javax.imageio.ImageIO;
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JButton;
+
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.image.BufferedImage;
+
 import javax.swing.JLabel;
+
 import java.awt.Font;
+import java.io.File;
+import java.io.IOException;
+
 import javax.swing.SwingConstants;
 
 
@@ -46,17 +56,33 @@ public class Test extends JFrame {
 		JLabel label = new JLabel("");
 		label.setHorizontalAlignment(SwingConstants.CENTER);
 		label.setFont(new Font("Tahoma", Font.PLAIN, 18));
-		label.setBounds(112, 144, 200, 50);
+		label.setBounds(119, 200, 200, 50);
 		contentPane.add(label);
 		
-		JButton btnHello = new JButton("Hello.");
-		btnHello.addActionListener(new ActionListener() {
+		JButton helloButton = new JButton("Hello.");
+		
+		
+
+		helloButton.setBorder(BorderFactory.createEmptyBorder());
+		helloButton.setContentAreaFilled(false);
+		BufferedImage buttonIcon;
+		try {
+			buttonIcon = ImageIO.read(new File("C:/Users/Henry/git/MegaKlick/content/circ.png"));
+			helloButton = new JButton(new ImageIcon(buttonIcon));
+		} 
+		catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		helloButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				label.setText("Hi.");
 			}
 		});
-		btnHello.setBounds(168, 112, 89, 23);
-		contentPane.add(btnHello);
+		helloButton.setBounds(10, 11, 64, 64);
+		contentPane.add(helloButton);
+		
 		
 		
 		

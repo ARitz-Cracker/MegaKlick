8f5c5904d340355cf6f4fdd7aeb17bb3f2ac07a7
Aritz Beobide-Cardinal <aritz@aritzcracker.ca>
2015-05-04 19:06:13 -0400

    * Added all shapes to the game * Clicking the incorrect shape will now loose points and lives * Added score screen

---
+++ MegaKlickProject/src/megak/GameShape.java
@@ -0,0 +1,25 @@
+package megak;
+
+import javax.swing.JButton;
+
+public class GameShape extends JButton {
+	int id;
+	boolean correct;
+	public void SetID(int val){
+		id = val;
+	}
+	public int GetID(){
+		return id;
+	}
+	public void SetCorrect(boolean val){
+		correct = val;
+	}
+	public boolean IsCorrect(){
+		return correct;
+	}
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -8034845604833830878L;
+
+}
--- MegaKlickProject/src/megak/VariableTimerTask.java
+++ MegaKlickProject/src/megak/VariableTimerTask.java
@@ -5,7 +5,6 @@ import java.util.TimerTask;
 
 class VariableTimerTask extends TimerTask  {
      String param;
-
      public VariableTimerTask(String param) {
          this.param = param;
      }
